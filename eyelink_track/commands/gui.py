f9548034ebaa487f033809d552809196e57f900c
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-10-16 17:51:17 +0200

    add mock argument to GUI command line

--- eyelink_track/commands/gui.py
+++ eyelink_track/commands/gui.py
@@ -1,3 +1,5 @@
+import argparse
+
 from qtpy.QtWidgets import QApplication
 
 from .._gui import GUI
@@ -5,6 +7,16 @@ from .._gui import GUI
 
 def run():
     """Run the eyelink-track GUI."""
+    parser = argparse.ArgumentParser(
+        prog=f"{__package__.split('.')[0]}-sys_info", description="gui"
+    )
+    parser.add_argument(
+        "--mock",
+        help="use a mock eye-tracker",
+        action="store_true",
+    )
+    args = parser.parse_args()
     app = QApplication([])
-    window = GUI()  # noqa: F841
+    kwargs = dict(host_ip=None) if args.mock else dict()
+    window = GUI(**kwargs)  # noqa: F841
     app.exec()
