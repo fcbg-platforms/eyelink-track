db829bf6afd69b3d29fb0cd112a285db832f1129
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-11-03 14:39:32 +0100

    fix pydocstyle configuration

--- eyelink_track/utils/qt.py
+++ eyelink_track/utils/qt.py
@@ -5,12 +5,14 @@ class SignalBlocker(QSignalBlocker):
     """Wrapper to use QSignalBlocker as a context manager in PySide2."""
 
     def __enter__(self):
+        """Enter the context manager."""
         if hasattr(super(), "__enter__"):
             super().__enter__()
         else:
             super().reblock()
 
     def __exit__(self, exc_type, exc_value, traceback):
+        """Exit the context manager."""
         if hasattr(super(), "__exit__"):
             super().__exit__(exc_type, exc_value, traceback)
         else:
