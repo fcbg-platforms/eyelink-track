f5395e03236ace7641dcccc9dbd2ddf0bb9a5d4b
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-10-30 15:12:35 +0100

    remove unneeded argument from CLI

--- eyelink_track/commands/track.py
+++ eyelink_track/commands/track.py
@@ -8,23 +8,19 @@ import click
 
 @click.command(name="track")
 @click.argument(
-    "dir",
-    type=click.Path(exists=True, dir_okay=True, file_okay=False, path_type=Path),
-    default=Path.cwd(),
-)
-@click.argument(
     "fname",
     type=click.Path(exists=False, dir_okay=False, file_okay=True, path_type=Path),
-    default=datetime.now().strftime("%H%M%S"),
+    default=Path.cwd() / datetime.now().strftime("%H%M%S"),
 )
-@click.option("--screen", help="ID of the screen to use.", type=int)
-def run(dir: Path, fname: Path, screen: int) -> None:  # noqa: A002
+@click.option("--screen", help="ID of the screen to use.", type=int, default=0)
+def run(fname: Path, screen: int) -> None:
     """Run track() command."""
     from ..eye_link import Eyelink
 
-    eye_link = Eyelink(dir, fname, screen=screen)
+    eye_link = Eyelink(fname.parent, fname.name, screen=screen)
     eye_link.calibrate()
     eye_link.win.close()
     eye_link.start()
     input(">>> Press ENTER to stop the recording.")
     eye_link.stop()
+
