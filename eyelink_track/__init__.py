8ef32495513e48cae458259ffd5b3638dfbf5ffa
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2024-10-30 13:53:03 +0100

    fix tests and imports

--- eyelink_track/__init__.py
+++ eyelink_track/__init__.py
@@ -1,4 +1,4 @@
-from . import config, eye_link, utils
+from . import config, utils
 from ._version import __version__
 from .utils.config import sys_info
 from .utils.logs import add_file_handler, logger, set_log_level
